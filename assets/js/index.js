2917c280b7412cadca0b0388610d90899ba3b40c
patriciax <yively.pa@gmail.com>
2020-03-04 16:44:30 -0400

    texto

--- assets/js/index.js
+++ assets/js/index.js
@@ -77,3 +77,46 @@ $(function(){
 		            .addClass('modal-dialog ' + $(e.relatedTarget).data('ui-class'))
 	})
 })
+
+
+
+
+
+
+// slick servicions
+$('.slider-services').slick({
+  infinite: true,
+  autoplay: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  fade: true,
+  cssEase: "linear",
+  arrows: true,
+  responsive: [{
+      breakpoint: 1200,
+      settings: {
+      
+        infinite: true,
+        dots: false
+      }
+    },
+    {
+      breakpoint: 900,
+      settings: {
+
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        dots: false,
+        autoplay: false,
+        arrows: false,
+        autoplaySpeed: 1000
+      }
+    }
+  ]
+});
